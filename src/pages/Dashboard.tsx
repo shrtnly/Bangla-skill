63b90c5197ec1fd5161d3b6788d120ecdbc335e6
freeman.find <freeman.find@gmail.com>
2025-11-06 23:58:29 +0000

    update

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -37,7 +37,7 @@ const translations = {
   bn: {
     myCourses: "আমার কোর্স",
     myProfile: "আমার প্রোফাইল",
-    learning: "শেখা",
+    learning: "শিখুন",
     coursesTitle: "আমার কোর্সসমূহ",
     allCourses: "সব কোর্সসমূহ",
     unfinished: "আনফিনিশড",
